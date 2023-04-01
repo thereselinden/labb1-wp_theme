ddee37864cad2d72753d24029a55643421bb3b5e
Therese Linden <theresemlinden@gmail.com>
2023-04-01 18:38:47 +0200

    added content to front page

--- front-page.php
+++ front-page.php
@@ -1,3 +1,30 @@
 <?php get_header()?>
-<?php echo '<h2>' . 'Hello from front-page' . '</h2>'?>
+
+<main>
+        <section>
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-12">
+                <div class="hero">
+                <?php if (have_posts()) {
+          while (have_posts()) {
+            the_post(); 
+            the_content(); 
+          }
+        } ?>
+                  <!-- <img src="img/city.jpg" />
+                  <div class="text">
+                    <h1>Hej och välkommen!</h1>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed sodales mauris. Aliquam felis est, efficitur vel fringilla quis, vehicula quis ex.
+                    </p>
+                  </div> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+
+
 <?php get_footer()?>
