da54ac07a93ae601e8a849c183038f8e5c9fcee3
Therese Linden <theresemlinden@gmail.com>
2023-04-02 20:31:58 +0200

    removed comments

--- front-page.php
+++ front-page.php
@@ -1,30 +1,23 @@
 <?php get_header()?>
 
 <main>
-        <section>
-          <div class="container">
-            <div class="row">
-              <div class="col-xs-12">
-                <div class="hero">
-                <?php if (have_posts()) {
-          while (have_posts()) {
-            the_post(); 
-            the_content(); 
-          }
-        } ?>
-                  <!-- <img src="img/city.jpg" />
-                  <div class="text">
-                    <h1>Hej och välkommen!</h1>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed sodales mauris. Aliquam felis est, efficitur vel fringilla quis, vehicula quis ex.
-                    </p>
-                  </div> -->
-                </div>
-              </div>
-            </div>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="hero">
+            <?php if (have_posts()) {
+              while (have_posts()) {
+                the_post(); 
+                the_content(); 
+              }   
+              } ?>
           </div>
-        </section>
-      </main>
+        </div>
+      </div>
+    </div>
+  </section>
+  </main>
 
 
 <?php get_footer()?>
