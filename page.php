7f9d5f9766f071cfd1bdd03029e51d07f373cf84
Therese Linden <theresemlinden@gmail.com>
2023-04-03 10:38:43 +0200

    added page.php to render the other pages, loop to print post content

--- page.php
+++ page.php
@@ -1,4 +1,41 @@
 <?php get_header()?>
 <h1><?php wp_title($sep = '')?></h1>
-<?php echo 'Hello from Page.php' ?>
+<?php echo 'Hello from Page.php'  ?>
+
+<main>
+			<section>
+				<div class="container">
+					<div class="row">
+						<div id="primary" class="col-xs-12 col-md-9">
+            <?php if (have_posts()) {
+              while (have_posts()) { ?>
+              <h1> <?php the_title(); ?> </h1> 
+              <?php  
+                the_post(); 
+                the_content(); 
+              }   
+              } ?>
+						</div>
+						<aside id="secondary" class="col-xs-12 col-md-3">
+            <?php wp_nav_menu(['theme_location' => 'aside-menu']); ?>
+							<!-- <ul class="side-menu">
+								<li>
+									<a href="undersida.html">Undersida</a>
+								</li>
+								<li>
+									<a href="undersida2.html">Undersida 2</a>
+								</li>
+								<li>
+									<a href="undersida3.html">Undersida 3</a>
+								</li>
+								<li>
+									<a href="undersida4.html">Undersida 4</a>
+								</li>
+							</ul> -->
+						</aside>
+					</div>
+				</div>
+			</section>
+		</main>
+
 <?php get_footer() ?>
