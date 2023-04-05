bb2811e01b70ed1529113f03f220cd2ccc93cb7e
Therese Linden <theresemlinden@gmail.com>
2023-04-05 19:52:20 +0200

    formatting code

--- page-undersida.php
+++ page-undersida.php
@@ -5,6 +5,7 @@
 		<div class="container">
 			<div class="row">
 				<div id="primary" class="col-xs-12 col-md-9">
+
 					<?php if (have_posts()) {
 						while (have_posts()) { ?>
 							<h1> <?php the_title(); ?> </h1>
@@ -13,6 +14,7 @@
 							the_content();
 						}
 					} ?>
+
 				</div>
 				<aside id="secondary" class="col-xs-12 col-md-3">
 					<?php wp_nav_menu(['theme_location' => 'aside-menu']); ?>
