4f6315424262dcd1721deef9992ff3de5237a7bc
Therese Linden <theresemlinden@gmail.com>
2023-04-05 16:09:35 +0200

    removed parts of code and imported it from template parts instead

--- home.php
+++ home.php
@@ -7,35 +7,11 @@
           <h1><?php wp_title($sep = '') ?></h1>
 
           <?php while (have_posts()) {
-            the_post(); ?>
-            <article>
-              <?php if (is_home()) {  ?>
-                <!-- <?php the_post_thumbnail(array(636, 424)); ?> -->
-                <?php the_post_thumbnail(); ?>
+            the_post();
+            get_template_part('template-parts/post-excerpt');
+          } ?>
 
-                <h2>
-                  <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-                </h2>
-                <ul class="meta">
-                  <li>
-                    <i class="fa fa-calendar"></i>
-                    <?php echo get_the_date(); ?>
-                  </li>
-                  <li>
-                    <i class="fa fa-user"></i>
-                    <a href=" <?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"> <?php the_author(); ?></a>
-                  </li>
-                  <li>
-                    <i class="fa fa-tag"></i>
-                    <?php the_category($separator = ', '); ?>
-                  </li>
-                </ul>
-              <?php
-                the_excerpt();
-              } ?>
-            </article>
-          <?php } ?>
-
+          <?php the_posts_pagination(); ?>
 
         </div>
         <aside id="secondary" class="col-xs-12 col-md-3">
@@ -44,9 +20,6 @@
               <li>
                 <form id="searchform" class="searchform">
                   <div>
-                    <!-- <label class="screen-reader-text">Sök efter:</label>
-                    <input type="text" />
-                    <input type="submit" value="Sök" /> -->
                     <?php dynamic_sidebar('aside_area_one'); ?>
                   </div>
                 </form>
