05ef1e9b42a5e8fbf33ff9e735650743b1d5679b
Therese Linden <theresemlinden@gmail.com>
2023-04-05 19:23:50 +0200

    reuse aside-bar code from template-parts

--- author.php
+++ author.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+
 <main>
   <section>
     <div class="container">
@@ -10,86 +11,13 @@
             the_post();
             get_template_part('template-parts/post-excerpt');
           } ?>
-
-
           <?php the_posts_pagination(); ?>
 
         </div>
-        <aside id="secondary" class="col-xs-12 col-md-3">
-          <div id="sidebar">
-            <ul>
-              <li>
-                <form id="searchform" class="searchform">
-                  <div>
-                    <label class="screen-reader-text">Sök efter:</label>
-                    <input type="text" />
-                    <input type="submit" value="Sök" />
-                  </div>
-                </form>
-              </li>
-            </ul>
-            <ul role="navigation">
-              <li class="pagenav">
-                <h2>Sidor</h2>
-                <ul>
-                  <li class="page_item current_page_item">
-                    <a href="">Blogg</a>
-                  </li>
-                  <li class="page_item">
-                    <a href="">Exempelsida</a>
-                  </li>
-                  <li class="page_item">
-                    <a href="">Kontakt</a>
-                  </li>
-                  <li class="page_item page_item_has_children">
-                    <a href="">Om mig</a>
-                    <ul class="children">
-                      <li class="page_item">
-                        <a href="">Intressen</a>
-                      </li>
-                      <li class="page_item page_item_has_children">
-                        <a href="">Portfolio</a>
-                        <ul class="children">
-                          <li class="page_item">
-                            <a href="">Projekt 1</a>
-                          </li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="page_item">
-                    <a href="">Startsida</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <h2>Arkiv</h2>
-                <ul>
-                  <li>
-                    <a href="arkiv.html">oktober 2016</a>
-                  </li>
-                </ul>
-              </li>
-              <li class="categories">
-                <h2>Kategorier</h2>
-                <ul>
-                  <li class="cat-item">
-                    <a href="">Natur</a> (1)
-                  </li>
-                  <li class="cat-item">
-                    <a href="">Okategoriserade</a> (3)
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </aside>
+        <?php get_template_part('template-parts/aside-bar'); ?>
       </div>
     </div>
   </section>
 </main>
-
-
-
 
 <?php get_footer(); ?>
