2eac457c7df12d00da7d14c7604b48360311c2ad
Therese Linden <theresemlinden@gmail.com>
2023-04-11 09:04:49 +0200

    added title to page

--- page-kontakt.php
+++ page-kontakt.php
@@ -5,6 +5,7 @@
     <div class="container">
       <div class="row">
         <div class="col-xs-12 col-md-8 col-md-offset-2">
+          <h1><?php the_title(); ?></h1>
           <?php while (have_posts()) {
             the_post();
             the_content();
