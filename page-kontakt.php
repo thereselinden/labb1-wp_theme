b8ede39f85f4cd20077cef7b3723b669a9da18d1
Therese Linden <theresemlinden@gmail.com>
2023-04-11 09:03:40 +0200

    added plugin for contact form in wp looping content instead of static html code

--- page-kontakt.php
+++ page-kontakt.php
@@ -5,16 +5,10 @@
     <div class="container">
       <div class="row">
         <div class="col-xs-12 col-md-8 col-md-offset-2">
-          <h1>Kontakt</h1>
-          <form>
-            <label>Namn</label>
-            <input type="text" />
-            <label>E-post</label>
-            <input type="email" />
-            <label>Meddelande</label>
-            <textarea></textarea>
-            <input type="submit" value="Skicka" />
-          </form>
+          <?php while (have_posts()) {
+            the_post();
+            the_content();
+          } ?>
         </div>
       </div>
     </div>
