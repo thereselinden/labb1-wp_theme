e54861c975c0181fb7bdd4f04b4e687c0764c0fa
Therese Linden <theresemlinden@gmail.com>
2023-04-13 10:55:37 +0200

    added date file to render archive for dates

---
+++ date.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+<main>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div id="primary" class="col-xs-12 col-md-9">
+          <h1><?php wp_title($sep = '') ?></h1>
+
+          <?php while (have_posts()) {
+            the_post();
+            get_template_part('template-parts/post-excerpt');
+          } ?>
+          <?php the_posts_pagination(); ?>
+        </div>
+
+        <aside id="secondary" class="col-xs-12 col-md-3">
+          <?php get_sidebar(); ?>
+        </aside>
+      </div>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
