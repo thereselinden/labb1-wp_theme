6d22122ac0f9c9917ecc010453768cb2f4f58a9b
Therese Linden <theresemlinden@gmail.com>
2023-04-13 11:51:16 +0200

    changed id based on update on functions widget settings

--- sidebar.php
+++ sidebar.php
@@ -6,12 +6,12 @@
   </ul>
   <ul role="navigation">
     <li class="pagenav">
-      <?php dynamic_sidebar('aside_area_two'); ?>
+      <?php dynamic_sidebar('aside_area_one'); ?>
     </li>
     <li>
-      <?php dynamic_sidebar('aside_area_three'); ?>
+      <?php dynamic_sidebar('aside_area_two'); ?>
     </li>
     <li class="categories">
-      <?php dynamic_sidebar('aside_area_four'); ?>
+      <?php dynamic_sidebar('aside_area_three'); ?>
   </ul>
 </div>
