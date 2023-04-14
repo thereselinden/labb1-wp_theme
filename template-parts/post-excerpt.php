a40dfa6f12edad64079b125fec17c8cfbac8dea4
Therese Linden <theresemlinden@gmail.com>
2023-04-14 11:17:43 +0200

    added post thumbail url

--- template-parts/post-excerpt.php
+++ template-parts/post-excerpt.php
@@ -1,6 +1,9 @@
-<article class='post-excerpt'>
-  <?php the_post_thumbnail('full'); ?>
+<?php $image_id = get_post_thumbnail_id();
+$image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
+?>
 
+<article class='post-excerpt'>
+  <img src="<?php the_post_thumbnail_url() ?>" alt="<?php echo $image_alt ?>">
   <h2>
     <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
   </h2>
