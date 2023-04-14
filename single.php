1f109ca728790a6503172579de214728aca53302
Therese Linden <theresemlinden@gmail.com>
2023-04-14 11:17:21 +0200

    replace thumbnail with post thumbnail url

--- single.php
+++ single.php
@@ -1,3 +1,6 @@
+<?php $image_id = get_post_thumbnail_id();
+$image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
+?>
 <?php get_header(); ?>
 
 <main>
@@ -10,9 +13,8 @@
             while (have_posts()) {
               the_post(); ?>
               <article class="post-excerpt">
-
-                <?php the_post_thumbnail('full'); ?>
-                <h1><?php the_title(); ?></h1>
+                <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php echo $image_alt ?>">
+                <?php the_title('<h1>', '</h1>'); ?>
                 <?php get_template_part('template-parts/post-meta'); ?>
                 <?php the_content(); ?>
               </article>
@@ -24,7 +26,6 @@
         <aside id="secondary" class="col-xs-12 col-md-3">
           <?php get_sidebar(); ?>
         </aside>
-
       </div>
     </div>
   </section>
